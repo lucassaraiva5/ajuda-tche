d9a6bc4d825623c4371fa1ad64d49a926e9ecdd0
Lucas S <lucassaraiva5@hotmail.com>
2024-05-09 00:45:13 -0300

    Added email message

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -87,7 +87,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('success', 'Seu e-mail foi verificado com sucesso!');
 
         return $this->redirectToRoute('index_route');
     }
