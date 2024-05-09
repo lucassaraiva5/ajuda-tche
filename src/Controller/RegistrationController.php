69edf9fde67aff324724313bb4cb4282935d4a34
Lucas S <lucassaraiva5@hotmail.com>
2024-05-09 00:42:24 -0300

    Updated email

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -64,7 +64,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_categoria_index');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
@@ -89,6 +89,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('index_route');
     }
 }
