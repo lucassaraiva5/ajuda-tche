fd245a6d7c121cec08e6006e5eb12325654f25a7
Lucas S <lucassaraiva5@hotmail.com>
2024-05-14 11:14:05 -0300

    Added changes

--- src/Controller/TipoUnidadeController.php
+++ src/Controller/TipoUnidadeController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
