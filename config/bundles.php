8230bd27434ea0cb99d6a308839df19901132a69
endriwmsi <endriwmsi@gmail.com>
2024-05-15 14:06:01 -0300

    chore: add tailwindcss cli

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
